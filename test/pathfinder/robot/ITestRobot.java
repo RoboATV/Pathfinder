b8f45a243d5a9a23eec50855f83c23dab64cfb80
AHaberl <andreashaberl@gmx.net>
2015-04-08 10:54:26 +0200

    fixed tests

---
+++ test/pathfinder/robot/ITestRobot.java
@@ -0,0 +1,11 @@
+package pathfinder.robot;
+
+import java.util.Queue;
+
+public interface ITestRobot {
+
+	public void setDistances(Queue<Float> distances);
+	public void setOrientation(Orientation orientation);
+	
+	
+}
--- test/pathfinder/robot/TestRobot.java
+++ test/pathfinder/robot/TestRobot.java
@@ -1,10 +1,12 @@
 package pathfinder.robot;
 
-import lejos.robotics.SampleProvider;
+import java.util.Queue;
 
-public class TestRobot implements IRobot{
+
+public class TestRobot implements IRobot, ITestRobot{
 	
 	private Orientation orientation;
+	private Queue<Float> distances;
 
 	public TestRobot(){
 		orientation = Orientation.NORTH;
@@ -12,26 +14,51 @@ public class TestRobot implements IRobot{
 
 	@Override
 	public void rotateTurnArm(int degrees) {
-		// TODO Auto-generated method stub
-		
+				
 	}
 
-	@Override
-	public SampleProvider getDistanceProvider() {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 	@Override
 	public void rotate(int degrees) {
-		// TODO Auto-generated method stub
-		
+				
 	}
 
 	@Override
 	public Orientation getOrientation() {
 		return orientation;
+	}
+
+	@Override
+	public void travel(double distance) {
+			
+	}
+
+	@Override
+	public void stop() {
+				
+	}
+
+	@Override
+	public float getDistance() {
+		if(distances.isEmpty()){
+			return -1;
+		}
+		float distance = distances.remove();		
+		
+		return distance;
+	}
+
+	@Override
+	public void setDistances(Queue<Float> distances) {
+		this.distances = distances;
+	}
+
+	@Override
+	public void setOrientation(Orientation orientation) {
+		this.orientation = orientation;
 	}	
+	
+	
 	
 	
 }
