b7594ef9e6fbb04a221e3ab31277a03f06e535b2
AHaberl <andreashaberl@gmx.net>
2015-04-09 10:29:02 +0200

    skeletons for obstacle avoidance

--- src/pathfinder/AvoidObstacle.java
+++ src/pathfinder/AvoidObstacle.java
@@ -2,6 +2,7 @@ package pathfinder;
 
 import lejos.robotics.subsumption.Behavior;
 import pathfinder.configuration.Configuration;
+import pathfinder.map.Coordinate;
 import pathfinder.robot.Robot;
 
 public class AvoidObstacle implements Behavior{
@@ -31,4 +32,37 @@ public class AvoidObstacle implements Behavior{
 		this.suppressed = true;		
 	}
 
+	
+	
+	private boolean detectWall(){
+		//TODO 
+		return true;
+	}
+	
+	
+	private void turnRobot(){
+		//TODO
+	}
+	
+	
+	private void avoidObstacle(){
+		//TODO
+	}
+	
+	private int turnDirection(Coordinate left, Coordinate right){
+		//TODO
+		int turnAngle = 90;
+		
+		return turnAngle;
+	}
+	
+	private int horizontalDistance(Coordinate corner){
+		//TODO
+		return 0;
+	}
+	
+	private int verticalDistance(int obstacleDistance){
+		//TODO
+		return 0;
+	}
 }
