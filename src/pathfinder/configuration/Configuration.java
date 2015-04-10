0498bb8007eee3723f88e8a1601e37da046a7034
AHaberl <andreashaberl@gmx.net>
2015-04-10 20:57:08 +0200

    added obstacle size

--- src/pathfinder/configuration/Configuration.java
+++ src/pathfinder/configuration/Configuration.java
@@ -2,8 +2,8 @@ package pathfinder.configuration;
 
 public class Configuration {
 
-	public final static float WALLDISTANCE = 20;
-	
+	public final static float WALLDISTANCE = 20;	
+	public final static float OBSTACLE_SIZE = 30;	
 	
 	
 	
