eed005659a540cf85aa6268aa2fc34736fb3e284
AHaberl <andreashaberl@gmx.net>
2015-04-15 19:18:36 +0200

    added initial orientation

--- src/pathfinder/Main.java
+++ src/pathfinder/Main.java
@@ -3,6 +3,8 @@ package pathfinder;
 import lejos.robotics.subsumption.Arbitrator;
 import lejos.robotics.subsumption.Behavior;
 import pathfinder.location.Locator;
+import pathfinder.orientation.InitialOrientation;
+import pathfinder.orientation.TurnNotPossible;
 import pathfinder.robot.Robot;
 
 public class Main {
@@ -11,16 +13,24 @@ public class Main {
 		
 		Robot robot = new Robot();
 		Locator locator = new Locator(robot);
+		InitialOrientation initialOrientation = new InitialOrientation(robot);
 		
 		Behavior drive = new Drive(robot, locator);
 		Behavior avoidObstacle = new AvoidObstacle(robot, locator);
 		
 		Behavior[] behaviors = new Behavior[1];
 		behaviors[0] = drive;
-//		behaviors[1] = avoidObstacle;
+		behaviors[1] = avoidObstacle;
 		
-		Arbitrator arbi = new Arbitrator(behaviors);
-		arbi.start();
+		
+		try {
+			initialOrientation.alignRobot();
+		} catch (TurnNotPossible e) {
+			System.out.println(e.toString());
+		}
+		
+		Arbitrator arbitrator = new Arbitrator(behaviors);
+		arbitrator.start();
 		
 	}
 
