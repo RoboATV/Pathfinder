3cc327ed65e76b77f373bd038b821071302197a5
Tobias Engelmann <T.O.B.B.E@web.de>
2015-05-04 19:35:09 +0200

    Fix test robot.

--- test/pathfinder/robot/ITestRobot.java
+++ test/pathfinder/robot/ITestRobot.java
@@ -9,8 +9,5 @@ public interface ITestRobot extends IRobot{
 	public void setDistances(Queue<Float> distances);
 	public void setHeadings(Queue<Float> headings);
 	public void setOrientation(Orientation orientation);
-	public void setColors(Queue<Float> colors);
-	public void setLight(Queue<Float> light);
-	
 	
 }
